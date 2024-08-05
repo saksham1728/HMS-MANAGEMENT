d4c36027b6f698a6bfb511663d98521ff11f308e
saksham1728 <148770428+saksham1728@users.noreply.github.com>
2024-08-05 23:02:54 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
       <form onSubmit={handleLogin}>
         <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email"/>
         <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password"/>
-        <input type="password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} placeholder="Confirm Password"/>
+     
         <div style={{gap:"10px",justifyContent:"flex-end", flexDirection:"row"}}>
           <p style={{marginBottom: 0}}>Not Registered </p>
           <Link to={"/register"} style={{textDecoration:"none",alignItems:"center"}}>Register Now</Link>
