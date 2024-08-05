d5d135f8c109aeb625ebf9fe5651afb88e3ee48c
saksham1728 <148770428+saksham1728@users.noreply.github.com>
2024-08-05 23:08:30 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(fileUpload({
     useTempFiles:true,
-    tempFileDir:"/temp/",
+    tempFileDir:"/tmp/",
 }));
 
 app.use("/api/v1/message",messageRouter);
