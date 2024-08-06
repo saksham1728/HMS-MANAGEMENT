1a151e55d2d18e25e431899c621dd27a9172611b
saksham1728 <148770428+saksham1728@users.noreply.github.com>
2024-08-06 19:32:25 +0530

    Update Biography.jsx

--- frontend/src/components/Biography.jsx
+++ frontend/src/components/Biography.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Biography = ({imageUrl}) => {
+const Biography = ({ imageUrl, className }) => {
   return (
-    <div className="container biography">
-        <div className="banner">
-            <img src={imageUrl} alt="about image" />
-        </div>
-        <div className="banner">
-            <p>
-                Biography
-            </p>
-            <h3>Who we are ?</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime vel a, labore dolorem dolore id. Animi error quidem atque aspernatur praesentium, ex voluptatibus repellat soluta quibusdam itaque, rem labore quasi.
-            </p>
-            <p>Lorem ipsum dolor, sit amet consectetu.</p>
-            <p>Lorem ipsum dolet.</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi dicta alias omnis, molestiae quaerat, excepturi natus iure aliquam aspernatur asperiores libero cumque suscipit quo quod possimus laborum facere adipisci unde.</p>
-
-        </div>
-      
+    <div className={`container biography ${className}`}>
+      <div className="banner">
+        <img src={imageUrl} alt="about image" />
+      </div>
+      <div className="banner">
+        <h3>Features We Provide</h3>
+        <p>Get your appointments online in a very fast way without standing in long queues.</p>
+        <p>Secure messaging on our portal allows you to send us a message if you want to report or request something from the hospital.</p>
+        <p>Our hospital boasts highly qualified doctors who are experts in their fields, ensuring you receive the best care.</p>
+        <p>We have integrated a chatbot that can help you by providing medicine recommendations for small issues like fever, cough, and more.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Biography
+Biography.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+export default Biography;
