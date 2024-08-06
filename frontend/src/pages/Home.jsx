bbf6f4faf74d825d0aabe1e5152a3b41de886ee4
saksham1728 <148770428+saksham1728@users.noreply.github.com>
2024-08-06 19:33:03 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Biography from '../components/Biography'
-import Departments from '../components/Departments'
-import MessageForm from '../components/MessageForm'
-
-
+import React from 'react';
+import Hero from '../components/Hero';
+import Biography from '../components/Biography';
+import Departments from '../components/Departments';
+import MessageForm from '../components/MessageForm';
 
 const Home = () => {
   return (
     <div>
-      
-      <Hero title={"Welcome to CANS Multi Specialty Hospital"} imageUrl={"./hero.png"}/>
-      <Biography imageUrl={"./about.png"}/>
+      <Hero title={"Welcome to CANS Multi Specialty Hospital"} imageUrl={"./hero.png"} />
+      <Biography 
+        imageUrl={"./feature.png"} 
+        className="home-biography" 
+      />
       <Departments />
       <MessageForm />
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
